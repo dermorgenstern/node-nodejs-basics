79345d0f7be8c0977bee95df2029bf9231c7e8a5
Eugene B <diemorgenstern@gmail.com>
2022-11-30 12:41:05 -0500

    Feat: fs/create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import { promises as fsasync } from "fs"
+const fs = import('fs');
+const checkIfFileExists = async path => !!(await fsasync.stat(path).catch(e => false));
+const createFile = async path => !!(await fsasync.writeFile(path, "I am fresh and young").catch(e => false));
+
+
 const create = async () => {
-    // Write your code here 
+    const path = "./src/fs/files/fresh.txt";
+    const exists = await checkIfFileExists(path);
+    if (exists)
+    {
+        throw new Error("FS operation failed");
+    }
+    else
+    {
+        createFile(path);
+    }
 };
 
 await create();
